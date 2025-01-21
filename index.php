8ad4e3dc3239a8425bb0080b0b6e79c335aaa649
Khushimainali <khushimainali05@gmail.com>
2025-01-21 21:09:20 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include "header.php"
+?>
+<style>
+   .home-img img {
+    width: 100%; /* Makes the image responsive */
+    height: auto; /* Maintains aspect ratio */
+    max-width: 500px; /* Limits the max width to 500px */
+    max-height: 600px; /* Limits the max height to 600px */
+    object-fit: cover; /* Ensures the image covers the container without distortion */
+    margin-left:80px;
+}
+
+</style>
+<section class="home">
+    <div class="text">
+        <h1>Looking for a rental clothes. <br>look no more!</h1>
+        <p>Fast and Easy Process</p>
+        <p>Easy Payment</p>
+        <p>Quality Assurance</p>
+    </div>
+    <div class="home-img">
+        <img src="il_1080xN.3229375443_rcwz.avif" alt="">
+    </div>
+</section>
+
+<?php 
+    include "footer.php"
+?>
